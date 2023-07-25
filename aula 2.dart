9408cf2b557ef61592bbb538c0f17078cca23e4a
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-07-25 11:53:37 -0300

    Add files via upload

---
+++ aula 2.dart
@@ -0,0 +1,91 @@
+//Aula 2 - curso de verão flutter
+
+void main() {
+  
+  //exercício 1
+  double numero1 = 7.2;
+  double numero2 = 6.8;
+  double valor;
+  
+  //soma
+  valor = numero1 + numero2;
+  print('Soma - $valor');
+  
+  //subtração
+  valor = numero1 - numero2;
+  print('Subtração - $valor');
+  
+  //multiplicação
+  valor = numero1 * numero2;
+  print('Multiplicação - $valor');
+  
+  //divisão
+  valor = numero1 / numero2;
+  print('Divisão - $valor');
+  
+  //resto da divisão
+  valor = numero1 % numero2;
+  print('Resto da divisão - $valor');
+  
+  print('---------------------------------');
+  
+  //exercício 2
+  int numero = 54;
+  print('numero $numero');
+  
+  if (numero % 2 == 0)
+    print('Esse número é par!');
+  
+  else
+    print('Esse número é impar!');
+  
+  print('---------------------------------');
+  
+  //exercício 3
+  //int numero = 43;
+  
+  //if (numero > 10 && numero < 20)
+  //  print('O numero está entre 10 e 20');
+  //if (numero == 0 || numero == 50)
+  //  print('O numero é igual a $numero');
+ // if (numero != 100 || numero == 200)
+ //   print('O numero é igual a $numero');
+  
+  
+  print('---------------------------------');
+  //exercício 4
+  int idade = 17;
+  
+  if (idade >= 18)
+    print('Você é maior de idade!');
+  else
+    print ('Você é menor de idade!');
+  
+  print('---------------------------------');
+  //exercício 5
+  int diaSemana = 4;
+  
+  switch(diaSemana){
+    case 1:
+      print('Hoje é segunda-feira.');
+      break;
+    case 2:
+      print('Hoje é terça-feira.');
+      break;
+    case 3:
+      print('Hoje é quarta-feira.');
+      break;
+    case 4:
+      print('Hoje é quinta-feira.');
+      break;
+    case 5:
+      print('Hoje é sexta-feira.');
+      break;
+    case 6:
+      print('Hoje é sábado.');
+      break;
+    case 7:
+      print('Hoje é domingo.');
+      break;
+  }
+}
