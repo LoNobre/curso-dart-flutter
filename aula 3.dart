2fabe7ef21f8f7b3f10d4c96e7f5f99b4615a05b
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-07-26 10:40:03 -0300

    Add files via upload

---
+++ aula 3.dart
@@ -0,0 +1,8 @@
+void main() {
+  // exercício 1
+  List <int> contagemFoguete = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1,];
+  for (var i = 0; i < contagemFoguete.length; i++)
+    print('Contagem regressiva: ${contagemFoguete[i]}');
+  
+  print('----------------------------------');
+}
