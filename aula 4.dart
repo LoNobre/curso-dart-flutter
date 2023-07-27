fc7db7e4b71fb4bc576b90a6256ae875c9d15eaa
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-07-27 10:33:30 -0300

    Add files via upload

---
+++ aula 4.dart
@@ -0,0 +1,46 @@
+
+class Carro{
+  String marca;
+  String modelo;
+  String ano;
+  String cor;
+  
+
+  Carro({
+    this.marca = 'Hundai',
+    this.modelo = 'Hb20',
+    this.ano = '2022',
+    this.cor = 'Branco',
+  });
+
+  void apresentaCarro(){
+  print('Dados do carro: $marca $modelo, $ano, $cor');
+}
+}
+
+class Moto{
+  String marca;
+  String modelo;
+  String ano;
+  String cor;
+
+  Moto({
+    this.marca = 'Honda',
+    this.modelo = 'Biz 110i',
+    this.ano = '2022',
+    this.cor = 'Prata',
+  });
+  
+  void apresentaMoto(){
+  print('Dados da moto: $marca, $modelo, $ano, $cor');
+}
+}
+
+
+void main() {
+  Carro carro = Carro();
+  carro.apresentaCarro();
+  
+  Moto moto = Moto();
+  moto.apresentaMoto();
+}
