d8ae1d468f336b3b95fe66a524f0f397e9ad2bc1
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-02 10:15:01 -0300

    Update aula 6.dart
    Inserção do exercício 4

--- aula 6.dart
+++ aula 6.dart
@@ -141,6 +141,75 @@ class MainApp extends StatelessWidget {
 
 /*
 //exercício 4
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  MainApp({super.key});
+
+  final Map<String, List<String>> dados = {
+    'Sobremesas': [
+      'Torta de Maçã',
+      'Mousse de Chocolate',
+      'Pudim de Leite Condensado',
+    ],
+    'Pratos principais': [
+      'Frango Assado com Batatas',
+      'Espaguete à Bolonhesa',
+      'Risoto de Cogumelos',
+    ],
+    'Aperitivos': [
+      'Bolinhos de Queijo',
+      'Bruschetta de Tomate e Manjericão',
+      'Canapés de Salmão com Cream Cheese',
+    ],
+  };
+
+  final String? categoriaPratos = 'Sobremesas'; //'Pratos principais' 'Aperitivos'
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Minhas receitas"),
+          backgroundColor: const Color.fromARGB(0, 114, 114, 207),
+        ),
+        body: Column(
+          children: dados.entries.map((categoria) {
+            if (categoriaPratos == null || categoriaPratos == categoria.key){
+            return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch, 
+            children: [
+              Center(
+                child: Text(categoria.key, 
+                style: const TextStyle(
+                  fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+                for (String value in categoria.value)
+                Text( value,
+                style: const TextStyle(fontSize: 18),
+                ),
+            ],
+          );
+          }
+          else{
+            return Container();
+          }
+          },
+          ).toList(),
+        ),
+      ),
+      );
+  }
+}
+
 
 */
 
