a566a8b4c72c1767b6ef1f082c8e83d32512d0c9
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-05 12:02:41 -0300

    Update aula 3.dart
    Adicionando a resolução do exercício 2

--- aula 3.dart
+++ aula 3.dart
@@ -5,4 +5,48 @@ void main() {
     print('Contagem regressiva: ${contagemFoguete[i]}');
   
   print('----------------------------------');
+
+  //exercicio 2
+
+  int numero = Random().nextInt(10) + 1;
+  int inicio = 1;
+  int tentativa = 3;
+  bool acerto = false;
+
+  while(inicio <= tentativa){
+    print('Tentativa $inicio, insira um palpite:');
+    String? input = stdin.readLineSync();
+
+    int valorInserido = int.parse(input!);
+
+    if (valorInserido == numero){
+      acerto = true;
+      print('Parabéns, você acertou!');
+      break;
+    }
+    else if (numero < valorInserido){
+        print('O número é menor que o palpite!');
+    }
+    else if (numero > valorInserido){
+        print('O número é maior que o palpite');
+      }
+    else{
+        print('Tentativa expirada');
+        print('Faltam $inicio tentativas');
+      }
+      inicio++;
+    }
+
+    if (acerto){
+      print('Você acertou.');
+    }
+    else {
+      print('Fim de jogo.');
+    }
+
+  print('---------------------------------------------------------------------------------------');
+
+  //exercício 3
+
+  
 }
