252ec9dce46bd7d09565739116f1a3ae92ebe885
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-08 11:49:00 -0300

    Update aula 5.dart
    Adicionando a resolução do exercício 2

--- aula 5.dart
+++ aula 5.dart
@@ -8,3 +8,29 @@ Future<String> fetchData() async {
 void main() async {
   await fetchData().then((dados) => print(dados));
 }
+
+print('------------------------------------------------------------');
+
+//Exercício2
+
+import 'dart:async';
+
+Stream<int> countDownStream(int max) async*{
+  for (int i = max; i >= 1 ; i--){
+    yield i;
+    await Future.delayed(const Duration(seconds: 1));
+  }
+}
+
+void main() {
+  final Stream <int> count = countDownStream(5);
+  count.listen((number) {
+    print('Número: $number');
+  }).onDone(() {
+    print('Contagem concluída com sucesso.');
+  });
+}
+
+print('------------------------------------------------------------');
+
+//Exercício3
