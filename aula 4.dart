6f9ae194eba70a30df52a04772740ba74c13e193
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-07 21:53:56 -0300

    Update aula 4.dart
    Adicionando a resolução do exercício 4

--- aula 4.dart
+++ aula 4.dart
@@ -221,3 +221,121 @@ void main() {
 
 print('---------------------------------------------------------------------------------');
 //exercício 4
+abstract class Veiculo {
+  final String marca;
+  final String modelo;
+  final String ano;
+
+  const Veiculo({
+    required this.marca,
+    required this.modelo,
+    required this.ano,
+  });
+
+  void apresentaVeiculo() {
+    print('Dados do veículo ');
+    print('Marca: ${marca}');
+    print('Modelo: ${modelo}');
+    print('Ano: ${ano}');
+  }
+
+  double precoAdicional(double precoBase);
+}
+
+  class Carro extends Veiculo {
+    final int quilometragemAtual;
+    final int qdePortas;
+
+    const Carro({
+      required super.marca,
+      required super.modelo,
+      required super.ano,
+      required this.quilometragemAtual,
+      required this.qdePortas,
+    });
+
+    @override
+    void apresentaCarro(){
+      print('Informações do carro');
+      super.apresentaVeiculo();
+      print('Quilometragem: ${quilometragemAtual}');
+      print('Quantidade de portas: ${qdePortas}');
+
+      final String statusUso;
+      if (quilometragemAtual <  15000 ){
+        statusUso =  'Seminovo' ;
+      } else  if (quilometragemAtual >  15000  && quilometragemAtual <  20000 ){
+        statusUso =  'Usado' ;
+      } else {
+        statusUso =  'Antigo' ;
+      }
+      print ('Qualidade de uso: $statusUso.\n');
+    }
+
+    @override
+    double precoAdicional(double precoBase){
+      return precoBase + qdePortas * 1000 + quilometragemAtual * 0.01;
+    }
+  }
+
+  class Moto extends Veiculo {
+    final int cilindradas;
+    final bool partidaEletrica;
+
+    const Moto({
+      required super.marca,
+      required super.modelo,
+      required super.ano,
+      required this.cilindradas,
+      required this.partidaEletrica,
+    });
+
+    @override
+    void apresentaMoto() {
+      print('Informações da moto');
+      super.apresentaVeiculo();
+      print('Cilindradas: ${cilindradas}');
+
+      String categoriaMoto;
+      if (cilindradas < 125) {
+        categoriaMoto = 'Leve';
+      } else if (cilindradas > 125 && cilindradas < 500) {
+        categoriaMoto = 'Normal';
+      } else {
+        categoriaMoto = 'Esportiva';
+      }
+      print('Categoria: $categoriaMoto.');
+    }
+
+    @override
+    double precoAdicional(double precoBase){
+      double precoFinal = precoBase;
+      precoFinal += cilindradas * 0.05;
+      if (partidaEletrica) {
+        precoFinal += 500;
+      }
+      return precoFinal;
+    }
+  }
+
+void main() {
+  final Carro carro = Carro (
+    marca: 'Hundai',
+    modelo: 'Hb20',
+    ano: '2022',
+    quilometragemAtual: 37589,
+    qdePortas: 4,
+  );
+
+  carro.apresentaCarro();
+
+  final Moto moto = Moto (
+    marca: 'Honda',
+    modelo: 'Biz 110i',
+    ano: '2022',
+    cilindradas: 55,
+    partidaEletrica: true,
+  );
+
+  moto.apresentaMoto();
+}
