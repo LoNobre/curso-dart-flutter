496b06ca7932fb87ac5b0baafb565e58edc12a39
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-08 12:53:17 -0300

    Update aula 5 - exemplos.dart
    Adicionando exemplo extra

--- aula 5 - exemplos.dart
+++ aula 5 - exemplos.dart
@@ -86,3 +86,40 @@ void main() {
 
 }*/
 
+print('------------------------------------------------------------------------------------------------');
+
+// exemplo extra
+
+import 'dart:async';
+
+Future<void> main() async {
+  // Define um controlador de stream
+  StreamController<List<String>> streamController =
+  StreamController<List<String>>();
+
+  //ATENÇÃO AQUI
+  //Ouve todos os eventos da stream EM QUALQUER MOMENTO.
+  streamController.stream.listen((event) {
+    // Aloca todos os nomes do evento atual
+    List<String> nomes = event.map((nome) => nome).toList();
+    // Imprime todos os nomes alocados no evento atual
+    print(nomes);
+    // OBS: Se quiser imprimir o tamanho (em caracteres) de cada nome basta colocar .length
+    //List<int> lenghNomes = event.map((nome) => nome.length).toList();
+  });
+
+  // Adiciona um evento com os nomes
+  streamController.sink.add(["Joao", "Maria", "Jose", "Pedro", "Ana"]);
+
+  // Bloqueia a execução por 3 segundos
+  await Future.delayed(const Duration(seconds: 3));
+
+  // Adicionar um outro evento com novos nomes
+  streamController.sink.add(["Marcia", "Beto", "Patricia", "Maria", "Marcos"]);
+
+  // Bloqueia a execução por 2 segundos - Adicionar um outro evento com novos nomes
+  await Future.delayed(
+      const Duration(seconds: 2),
+          () => streamController.sink
+          .add(["Felipe", "Thiago", "Victor", "Larissa", "Thais"]));
+}
