1ad19d7de4e6224c957e237c9692b057a63d1de0
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-08 12:02:25 -0300

    Update aula 5.dart
    Adicionando a resolução do exercício 3

--- aula 5.dart
+++ aula 5.dart
@@ -34,3 +34,34 @@ void main() {
 print('------------------------------------------------------------');
 
 //Exercício3
+
+import 'dart:async';
+
+Future<int> futureOne() async {
+  await Future.delayed(const Duration(seconds: 5));
+  return 10 * 100;
+}
+
+Future<String> futureTwo() async {
+  await Future.delayed(const Duration(seconds: 2));
+  return 'Processando..';
+}
+
+Future<String> futureThree() async {
+  await Future.delayed(const Duration(seconds: 10));
+  return 'Operação concluída';
+}
+
+Future <void> main() async {
+  Future.wait([futureOne(), futureTwo(), futureThree()]).then((List<Object> reiceved) {
+    print('Dados do Future 1 ${reiceved[0]}');
+    print('Dados do Future 2 ${reiceved[1]}');
+    print('Dados do Future 3 ${reiceved[2]}');
+
+  });
+}
+
+print('------------------------------------------------------------');
+
+//Exercício4
+
