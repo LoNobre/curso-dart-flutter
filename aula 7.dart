4782f58049d7348d6a7def1ac18dbad3955d77b9
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-10 21:30:47 -0300

    Update aula 7.dart
    Adicionando exercício 3

--- aula 7.dart
+++ aula 7.dart
@@ -128,3 +128,97 @@ class MainApp extends StatelessWidget {
 */
 
 // exercício 3
+/*
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  MainApp({super.key});
+
+  final Map<String, List<String>> dados = {
+    'Sobremesas': [
+      'Torta de Maçã',
+      'Mousse de Chocolate',
+      'Pudim de Leite Condensado',
+    ],
+    'Pratos principais': [
+      'Frango Assado com Batatas',
+      'Espaguete à Bolonhesa',
+      'Risoto de Cogumelos',
+    ],
+    'Aperitivos': [
+      'Bolinhos de Queijo',
+      'Bruschetta de Tomate e Manjericão',
+      'Canapés de Salmão com Cream Cheese',
+    ],
+    'Bebidas': [
+      'Água',
+      'Cerveja',
+      'Refrigerante',
+    ]
+  };
+
+  final String? categoriaPratos = null;
+  final String busca = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Minhas receitas"),
+          backgroundColor: const Color.fromARGB(0, 114, 114, 207),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: dados.entries.map((categoria) {
+              if (categoriaPratos == null || categoriaPratos == categoria.key){
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    if(busca.isEmpty)
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: Text(categoria.key,
+                            style: const TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          for (String value in categoria.value)
+                            if(value.contains(busca) || busca.isEmpty)
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: Text( value,
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                              ),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }
+              else{
+                return Container();
+              }
+            },
+            ).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+/*
