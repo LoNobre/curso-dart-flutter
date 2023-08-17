7f61aacb9a42309661272eeeabdcf14fd3dd2e55
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-17 11:43:33 -0300

    Update aula 8.dart
    Ajuste de responsividade

--- aula 8.dart
+++ aula 8.dart
@@ -40,72 +40,102 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Minhas receitas"),
           backgroundColor: const Color.fromARGB(0, 114, 114, 207),
+          toolbarHeight: size.height * 0.1,
         ),
-        body: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'buscar',
-                hintText: 'Buscar por',
-              ), onChanged: (novaBusca){
-                setState((){
-                  busca = novaBusca;
-                });
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
+        body: SizedBox(
+          height: size.height * 0.9,
+          width: size.width,
+          child: Center(
+            child: SingleChildScrollView(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: dados.entries.map((categoria) {
-                  if (categoriaPratos == null || categoriaPratos == categoria.key){
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch, 
-                      children: [
-                        if(busca.isEmpty)
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          child: Center(
-                            child: Text(categoria.key, 
-                              style: const TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'buscar',
+                      hintText: 'Buscar por',
+                    ), onChanged: (novaBusca){
+                    setState((){
+                      busca = novaBusca;
+                    });
+                  },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: dados.entries.map((categoria) {
+                        if (categoriaPratos == null || categoriaPratos == categoria.key){
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
                             children: [
-                              for (String value in categoria.value)
-                                if(value.contains(busca) || busca.isEmpty)
-                                  SizedBox(
-                                    width: MediaQuery.of(context).size.width,
-                                    child: Text( value,
-                                      style: const TextStyle(fontSize: 18),
-                                    ),
+                              if(busca.isEmpty)
+                                Center(
+                                  child: Text(categoria.key,
+                                    style: const TextStyle(
+                                        fontSize: 24, fontWeight: FontWeight.bold),
                                   ),
+                                ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    for (String value in categoria.value)
+                                      if(value.contains(busca) || busca.isEmpty)
+                                        Expanded(
+                                          flex: 1,
+                                          child: Container(
+                                            margin: EdgeInsets.symmetric(vertical: 10.5, horizontal:  5.5),
+                                            padding: const EdgeInsets.all(8.0),
+                                            height: 60,
+                                            //width: 60,
+                                            decoration: BoxDecoration(
+                                              color: Colors.red[300],
+                                            ),
+                                            child: Column(
+                                              children: [
+                                                const Expanded(
+                                                  child: Icon(
+                                                    Icons.restaurant,
+                                                    size: 18,
+                                                    color: Colors.black38,
+                                                  ),
+                                                ),
+                                                Flexible(
+                                                  child:
+                                                  Text( value,
+                                                    style: TextStyle(fontSize: size.width * 0.012),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                  ],
+                                ),
+                              ),
                             ],
-                         ),
-                       ),
-                    ],
-                    );
-                  }
-                  else{
-                    return Container();
-                  }
-                },
-              ).toList(),
+                          );
+                        }
+                        else{
+                          return Container();
+                        }
+                      },
+                      ).toList(),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
-      );
+    );
   }
 }
